6f76a4a67a3d5abf7aab19aeaa909246b0054f97
TeqbM <mukesh@teqbuddies.com>
2024-10-10 17:08:08 +0530

    Backend Setup Nodejs

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+const express = require('express');
+const app = express();
+const port = 3030;
+
+app.get('/', (req, res) => {
+     res.end('<h1>dede</h1>');
+})
+
+app.listen(port, (req, res) => {
+     console.log('listening on ' + port);
+})
